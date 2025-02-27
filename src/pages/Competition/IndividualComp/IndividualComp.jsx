414f5af399dc5bc12e2a3a6541488c688ac6dfcb
Nayan <nayanunni95@gmail.com>
2025-02-27 20:27:02 +0530

    fix: syntax error resolved

--- src/pages/Competition/IndividualComp/IndividualComp.jsx
+++ src/pages/Competition/IndividualComp/IndividualComp.jsx
@@ -33,92 +33,90 @@ function IndividualComp() {
     setCompData(competition.find((value) => value.categoryId == compId));
   }, [compId]);
 
-
-
   return (
     <div>
-          <h3>Innerve'25 Presents</h3>
-          <h1 className={styles.compheading}>{compData && compData.name}</h1>
+      <div>
+        <h3>Innerve'25 Presents</h3>
+        <h1 className={styles.compheading}>{compData && compData.name}</h1>
+      </div>
+      <div className={styles.compContentSection}>
+        <div className={styles.contentPosterSection}>
+          <img src={compData && compData.url} alt={compData && compData.name} />
         </div>
-        <div className={styles.compContentSection}>
-          <div className={styles.contentPosterSection}>
-            <img
-              src={compData && compData.url}
-              alt={compData && compData.name}
-            />
+        <div className={styles.contentDetailsSection}>
+          <div className={styles.contentAbout}>
+            <h2 className={styles.contentAbout}>About the event</h2>
           </div>
-          <div className={styles.contentDetailsSection}>
-            <div className={styles.contentAbout}>
-              <h2 className={styles.contentAbout}>About the event</h2>
-            </div>
-            <div className={styles.contentPara}>
-              <p>{compData && compData.about}</p> 
-              <h3 className={styles.compPrice}>
-                Contact Details
-                {compData &&
-                  compData.contact.map((value, index) => {
-                    return (
-                      <div
-                        key={index}
-                        style={{
-                          display: 'flex',
-                          margin: '0',
-                          color: 'gray',
-                          fontSize: '1rem',
-                        }}
-                      >
-                        <span>{value.name}</span>:<Link>{value.no}</Link>
-                      </div>
-                    );
-                  })}
-                {/* <span>for any enquires</span> */}
-              </h3>
-              {/* <h3 style={{ textAlign: 'center' }}>
+          <div className={styles.contentPara}>
+            <p>{compData && compData.about}</p>
+            <h3 className={styles.compPrice}>
+              Contact Details
+              {compData &&
+                compData.contact.map((value, index) => {
+                  return (
+                    <div
+                      key={index}
+                      style={{
+                        display: 'flex',
+                        margin: '0',
+                        color: 'gray',
+                        fontSize: '1rem',
+                      }}
+                    >
+                      <span>{value.name}</span>:<Link>{value.no}</Link>
+                    </div>
+                  );
+                })}
+              {/* <span>for any enquires</span> */}
+            </h3>
+            {/* <h3 style={{ textAlign: 'center' }}>
                 Note: <span>{compData && compData.note}</span>
               </h3> */}
-              <h3 className={styles.compPrice}>
-                Registration Fee: <span className={styles.mainRegPrice}>₹{compData && compData.price}</span>
-              </h3>
-              <h3 className={styles.compPrice}>
-                Price Pool: <span className={styles.mainPricePool}>₹{compData && compData.pricePool}</span>
-              </h3>
-            </div>
-            {/* <div className={styles.contentAdditional}>
+            <h3 className={styles.compPrice}>
+              Registration Fee:{' '}
+              <span className={styles.mainRegPrice}>
+                ₹{compData && compData.price}
+              </span>
+            </h3>
+            <h3 className={styles.compPrice}>
+              Price Pool:{' '}
+              <span className={styles.mainPricePool}>
+                ₹{compData && compData.pricePool}
+              </span>
+            </h3>
+          </div>
+          {/* <div className={styles.contentAdditional}>
               <label>Number of Participation</label>
               <select name="" id="">
                 <option value="three">3</option>
                 <option value="four">4</option>
               </select>
             </div> */}
-            <div className={styles.contentBtn}>
-              <Link>
-                <button onClick={() => navigate(compData && compData.regLink)}>
-                  {/* Register{' '} */}
-                  {compData && compData.isRegOpen ? (
-                    <>
-                      Register <FiArrowUpRight size={20} />
-                    </>
-                  ) : (
-                    'Registration Closed'
-                  )}{' '}
-                </button>
-              </Link>
-              <button
-                id={styles.shareBtn}
-                onClick={() =>
-                  shareItem(
-                    compData.name,
-                    `/competitions?compName=${compData.searchKey}`
-                  )
-                }
-              >
-                Share
-                <RiShareForwardFill
-                  size={20}
-                  style={{ marginLeft: '0.3rem' }}
-                />
+          <div className={styles.contentBtn}>
+            <Link>
+              <button onClick={() => navigate(compData && compData.regLink)}>
+                {/* Register{' '} */}
+                {compData && compData.isRegOpen ? (
+                  <>
+                    Register <FiArrowUpRight size={20} />
+                  </>
+                ) : (
+                  'Registration Closed'
+                )}{' '}
               </button>
-            </div>
+            </Link>
+            <button
+              id={styles.shareBtn}
+              onClick={() =>
+                shareItem(
+                  compData.name,
+                  `/competitions?compName=${compData.searchKey}`
+                )
+              }
+            >
+              Share
+              <RiShareForwardFill size={20} style={{ marginLeft: '0.3rem' }} />
+            </button>
           </div>
         </div>
       </div>
